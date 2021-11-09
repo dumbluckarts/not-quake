c20a96af44e5eb806ccc87ea17fb62fdef87b4fb
cakedev <calemaynes@gmail.com>
2021-11-08 17:52:09 -0700

    fixed velocity UI

--- entities/player/Velocity.gd
+++ entities/player/Velocity.gd
@@ -9,13 +9,13 @@ export var enabled := true
 func _physics_process(_delta: float) -> void:
 	if enabled:
 		var v: Vector3 = $"../..".velocity
-		var s = v.length()/_delta
+		var s = v.length()/(_delta*60)
 		if v == null:
 			text = "x: 0 y: 0 z: 0 speed: 0"
 			return
 		
-		text = "x: " + str(stepify(v.x, 0.01)) + " y: " + str(stepify(v.y, 0.01)) + " z: " + str(stepify(v.z, 0.01)) + " speed: " + str(stepify(s,0.01))
-		
+		#text = "x: " + str(stepify(v.x, 0.01)) + " y: " + str(stepify(v.y, 0.01)) + " z: " + str(stepify(v.z, 0.01)) + " speed: " + str(stepify(s,0.01))
+		text = str(stepify(s,0.01))
 	
 	else:
 		text = ""
