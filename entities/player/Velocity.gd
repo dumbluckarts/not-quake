c9a626a5ba58db78358c88c51a4eed6d70f1f017
Nelson M <nelson.mccullough@peloton.com>
2021-11-13 19:28:13 -0700

    for chale

--- entities/player/Velocity.gd
+++ entities/player/Velocity.gd
@@ -9,4 +9,5 @@ func _physics_process(_delta: float) -> void:
 	var y = stepify(p.global_transform.origin.y, 0.1)
 	var z = stepify(p.global_transform.origin.z, 0.1)
 	var s = stepify(v.length()/(_delta*60), 0.1)
-	text = "x: %s y: %s z: %s s: %s" % [str(x), str(y), str(z), str(s)]
+	#text = "x: %s y: %s z: %s s: %s" % [str(x), str(y), str(z), str(s)]
+	text = "v: %s" % str(s)
