b1cba872e1f23dfd31df818492637f92b2ce2399
Nelson M <nelson.mccullough@peloton.com>
2021-11-16 22:24:59 -0700

    visual update

--- scripts/interface/CanvasLayer.gd
+++ scripts/interface/CanvasLayer.gd
@@ -11,11 +11,4 @@ func _on_Combat_hit_damagable():
 	$Crosshair.add_child(hitmarker)
 
 func _on_Crosshair_draw():
-	var middle = get_viewport().get_visible_rect().size / 2
-	var points = [
-		Vector2(-8, 0),
-		Vector2(8, 0),
-		Vector2(0, -8), 
-		Vector2(0, 8)
-	]
-	$Crosshair.draw_multiline(points, Color.green, 2, true)
+	$Crosshair.draw_circle(Vector2.ZERO, 1, Color.green)
