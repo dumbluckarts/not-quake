4a580c1a164d3ff6e16f0fa82c7e4394778d7b81
Nelson M <nelson.mccullough@peloton.com>
2021-11-18 21:51:18 -0700

    pls

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -27,12 +27,27 @@ func _input(event: InputEvent) -> void:
 				mouse_mode = "CAPTURED"
 
 func _on_ready_client(data):
-	var enemy = load("res://godot/scenes/enemies/Enemy.tscn")
-	$Enemies.add_child(enemy.set_meta("id", data['id']))
+	var enemy = load("res://godot/scenes/enemies/Enemy.tscn").instance()
+	enemy.name = str(data['id'])
+	$Enemies.add_child(enemy)
 
-func _on_update_client(data):
-	for child in $Enemies.get_children():
-		var id = child.get_meta('id')
-		var pos = data[id]['data']['position']
+func _on_update_client(data: Dictionary):
+	for key in data.keys():
+		var pls = data[key]
+		var enemy = $Enemies.get_node(key)
 		
-		child.global_transform.origin = pos
+		if 'data' in pls: 
+			var pos = Vector3.ZERO
+			var pos_data = pls['data']['position']
+			var pos_split = pos_data.split(',')
+			
+			print(pos_split)
+			
+#			enemy.global_transform.origin = pos_data
+		
+		
+#	for child in $Enemies.get_children():
+#		var id = child.get_meta('id')
+#		var pos = data[id]['data']['position']
+#
+#		child.global_transform.origin = pos
