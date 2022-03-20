ecaac8b841a39745842d01239f5b888e3d4078e7
Nelson M <nelson.mccullough@peloton.com>
2022-03-19 21:24:32 -0600

    added sumb shit:

---
+++ client/Camera2D.gd
@@ -0,0 +1,15 @@
+extends Camera2D
+class_name Cam
+
+var enabled = true
+var shake_strength = 0
+var zoom_weight = 0.1
+
+onready var zoom_original = zoom
+
+func _physics_process(delta):
+	if not enabled: return
+	shake_strength = lerp(shake_strength, 0, 0.05)
+	global_position.x = lerp(global_position.x + rand_range(-shake_strength, shake_strength), $"../Player".global_position.x, 0.35)
+	global_position.y = lerp(global_position.y + rand_range(-shake_strength, shake_strength), $"../Player".global_position.y, 0.35)
+	zoom = zoom.linear_interpolate(zoom_original, zoom_weight)
---
+++ client/Player.gd
@@ -0,0 +1,9 @@
+extends KinematicBody2D
+
+func _process(delta):
+	$Gun.look_at(get_global_mouse_position())
+	$Sprite.flip_h = get_local_mouse_position().x < 0
+	$Gun.flip_v = get_local_mouse_position().x < 0
+	
+	if Input.is_action_pressed("shoot"):
+		$"../Camera2D".shake_strength = 2.0
