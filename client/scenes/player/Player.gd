d407bddc94e1ff2c11847b61bb085164818f91dc
nelsondev <nelsontron@outlook.com>
2022-04-02 17:37:42 -0600

    started another look? idk

--- client/scenes/player/Player.gd
+++ client/scenes/player/Player.gd
@@ -1,16 +1,10 @@
 extends KinematicBody2D
 export (PackedScene) var BULLET
-
-func _process(_delta):
-	$Gun.look_at(get_global_mouse_position())
-	$Sprite.flip_h = get_local_mouse_position().x < 0
-	$Gun.flip_v = get_local_mouse_position().x < 0
-	
-	if Input.is_action_pressed("shoot"):
-		shoot()
 	
 func _physics_process(delta):
 	$Movement.process(self, delta)
+
+
 
 # TODO: Move to gun?
 func shoot():
